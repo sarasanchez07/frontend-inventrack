fc476d828ee2d1a759a0f705a09456bedf4026c3
sarasanchez07 <estradasanchezsara2007@gmail.com>
2026-03-20 20:40:09 -0500

    elomianr diseños de esperar

--- src/pages/movements/MovementsPage.jsx
+++ src/pages/movements/MovementsPage.jsx
@@ -466,10 +466,7 @@ const MovementsPage = () => {
                                     </tbody>
                                 </table>
                             ) : (
-                                <div className="empty-state-admin">
-                                    <img src="https://cdn-icons-png.flaticon.com/512/7486/7486774.png" alt="No records" className="empty-icon" />
-                                    <p>No tiene registros</p>
-                                </div>
+                                <div></div>
                             )}
                         </div>
                     </div>
